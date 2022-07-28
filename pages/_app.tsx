4132f184192d21156997b8cfb426ec46e9d04032
Tiewly <tananporn2112@gmail.com>
2022-07-28 23:25:39 +0700

    Add boostrap in <Head>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+        ></link>
       </Head>
       <Navbar />
       <Component {...pageProps} />
